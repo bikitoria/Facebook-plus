0a899252bfb5989ce607551b5c44f4df5fb2c925
Victoria Piquer <victoria.piquer@ucalgary.ca>
2025-06-22 16:23:00 -0600

    Initial Commit

---
+++ FacebookClone/FacebookCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacebookCloneApp.swift
+//  FacebookClone
+//
+//  Created by Victoria Piquer on 2025-06-22.
+//
+
+import SwiftUI
+
+@main
+struct FacebookCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FacebookCloneTests/FacebookCloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  FacebookCloneTests.swift
+//  FacebookCloneTests
+//
+//  Created by Victoria Piquer on 2025-06-22.
+//
+
+import Testing
+@testable import FacebookClone
+
+struct FacebookCloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
