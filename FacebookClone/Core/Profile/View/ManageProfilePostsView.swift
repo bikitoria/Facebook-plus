d091f53ca2884bd87fc9d7d0567b67d5b11be353
Victoria Piquer <victoria.piquer@ucalgary.ca>
2025-07-02 18:42:50 -0600

    Completed profile view

---
+++ FacebookClone/Core/Profile/View/ManageProfilePostsView.swift
@@ -0,0 +1,18 @@
+//
+//  ManageProfilesView.swift
+//  FacebookClone
+//
+//  Created by Victoria Piquer on 2025-07-02.
+//
+
+import SwiftUI
+
+struct ManageProfilesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ManageProfilesView()
+}
---
+++ FacebookClone/Core/Profile/View/ReelReusableView.swift
@@ -0,0 +1,18 @@
+//
+//  ReelReusableView.swift
+//  FacebookClone
+//
+//  Created by Victoria Piquer on 2025-07-02.
+//
+
+import SwiftUI
+
+struct ReelReusableView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReelReusableView()
+}
